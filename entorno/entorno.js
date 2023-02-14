1966a287472116befb22b98d7e56b720228ff9a6
dbsalazarr <dbsalazarr@gmail.com>
2023-02-14 13:31:25 -0500

    Herramientas de node, nodemon PM2

--- entorno/entorno.js
+++ entorno/entorno.js
@@ -1,4 +1,7 @@
 let saludo = process.env.NOMBRE || "Davis"
 let web = process.env.WEB || "Sin definir"
+
 console.log("Hola "+saludo)
 console.log("Sitio Web: " + web)
+console.log("Mi name is Bremdow")
+console.log("SAlazarrrr")
