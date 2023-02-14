8df5c1fe30a6aaa47e3053678af6c07a4c243976
dbsalazarr <dbsalazarr@gmail.com>
2023-02-14 01:31:00 -0500

    Iniciando con Node JS

---
+++ entorno/entorno.js
@@ -0,0 +1,4 @@
+let saludo = process.env.NOMBRE || "Davis"
+let web = process.env.WEB || "Sin definir"
+console.log("Hola "+saludo)
+console.log("Sitio Web: " + web)
---
+++ monohilo/monohilo.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo")
+
+let i = 0;
+
+setInterval( function(){
+    console.log("Pero saludame, ya pasaron ..." + (i++) + "s" )
+
+    if( i == 5){
+        var a = 3 + z;
+    }
+}, 1000)
+
+console.log("Segunda ejecución")
